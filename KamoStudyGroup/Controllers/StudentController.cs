28b094330f5025cd9e3b1a58769e909624f272aa
Kamohelo-coder <kamomotsoko@gmail.com>
2025-03-02 18:16:12 +0200

    Add project files.

---
+++ KamoStudyGroup/Controllers/StudentController.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using KamoStudyGroup.Models; // Add your model namespace here
+
+namespace KamoStudyGroup.Controllers
+{
+    public class StudentController : Controller
+    {
+        // GET: Student
+        public ActionResult Index()
+        {
+            List<StudentDetails> students = new List<StudentDetails>  // Create a list of the students you wanna add
+            {
+                new StudentDetails      // Student details added in the list
+                {
+                    StudentNo = "22560646",
+                    StudentName = "Kamo",
+                    StudentSurname = "Motsoko",
+                    StudentEmail = "u22560646@tuks.co.za",
+                },
+
+                new StudentDetails
+                {
+                    StudentNo = "23799448",
+                    StudentName = "Ayola",
+                    StudentSurname = "Gwampi",
+                    StudentEmail = "u23799448@tuks.co.za",
+                },
+
+                new StudentDetails
+                {
+                    StudentNo = "23552311",
+                    StudentName = "Unathi",
+                    StudentSurname = "Mokgoetsi",
+                    StudentEmail = "u23552311@tuks.co.za",
+                },
+
+                new StudentDetails
+                {
+                    StudentNo = "23671922",
+                    StudentName = "Ntokoto",
+                    StudentSurname = "Mongwe",
+                    StudentEmail = "u23671922@tuks.co.za",
+                },
+
+                new StudentDetails
+                {
+                    StudentNo = "23533057",
+                    StudentName = "Xolani",
+                    StudentSurname = "Tobo",
+                    StudentEmail = "u23533057@tuks.co.za",
+                },
+            };
+            return View(students);  //Display list of students in the View
+        }
+    }
+}
---
+++ KamoStudyGroup/Models/StudentDetails.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace KamoStudyGroup.Models
+{
+    public class StudentDetails   //Create a class 
+    {
+        [DisplayName ("STUDENT NUMBER")]  //Display the name of your attributes on the table as STUDENT NUMBER, NAME, SURNAME, EMAIL ADDRESS
+        public string StudentNo { get; set; }  //Attributes added to the student table
+        [DisplayName ("NAME")]
+        public string StudentName { get; set;}
+        [DisplayName ("SURNAME")]
+        public string StudentSurname { get; set;}
+        [DisplayName ("EMAIL ADDRESS")]
+        public string StudentEmail { get; set;}
+
+    }
+}
